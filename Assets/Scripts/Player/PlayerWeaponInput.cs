93e8633d09693253b08a87c390bcaee153337b0b
Liad Halotzi <liadhalotzi@gmail.com>
2023-02-03 01:15:02 +0200

    Shooting projectiles

--- Assets/Scripts/Player/PlayerWeaponInput.cs
+++ Assets/Scripts/Player/PlayerWeaponInput.cs
@@ -8,15 +8,14 @@ namespace Player
         [SerializeField] GameObject _indicator;
         [SerializeField] GameObject _projectile;
         private PlayerInputSystem _playerInputSystem;
-        private Vector2 _idicatorRotation;
-        private Vector2 _projectileDirection;
+        private Vector2 _rotation;
         private float _angle;
 
-        public Vector2 ProjectileDirection => _projectileDirection;
         private void Awake()
         {
             _playerInputSystem = new PlayerInputSystem();
-            _playerInputSystem.Player.Rotate.performed += ctx => _idicatorRotation = ctx.ReadValue<Vector2>();
+            _playerInputSystem.Player.Rotate.performed += ctx => _rotation = ctx.ReadValue<Vector2>();
+            _playerInputSystem.Player.Rotate.performed += ctx => _rotation = ctx.ReadValue<Vector2>();
             _playerInputSystem.Player.Shoot.performed += ctx => ShootProjectile();
         }
 
@@ -27,17 +26,17 @@ namespace Player
 
         private void Rotate()
         {
-            if (_idicatorRotation.x == 0 && _idicatorRotation.y == 0)
+            if (_rotation.x == 0 && _rotation.y == 0)
                 return;
-            _projectileDirection = (new Vector2(_idicatorRotation.x, -_idicatorRotation.y) * Time.deltaTime * 100f).normalized / 100;
-            _angle = Mathf.Atan2(-_idicatorRotation.x, _idicatorRotation.y) * Mathf.Rad2Deg;
+            _angle = Mathf.Atan2(-_rotation.x, _rotation.y) * Mathf.Rad2Deg;
             _indicator.transform.rotation = Quaternion.AngleAxis(_angle, Vector3.forward);
         }
 
         private void ShootProjectile()
         {
-            Quaternion quaternion = new Quaternion(-_idicatorRotation.x, _idicatorRotation.y, 0, 0);
+            Quaternion quaternion = new Quaternion(-_rotation.x, _rotation.y, 0, 0);
             GameObject projectile = Instantiate(_projectile, _indicator.transform.position, quaternion);
+            projectile.transform.rotation = Quaternion.AngleAxis(_angle, Vector3.forward);
         }
 
         private void OnEnable()
--- Assets/Scripts/Projectile/Projectile.cs
+++ Assets/Scripts/Projectile/Projectile.cs
@@ -7,31 +7,21 @@ namespace Projectile
     {
         [SerializeField] private float _speed;
         [SerializeField] private int _damage;
+        [SerializeField] private float _maxLifeTime;
         private Vector3 _dirction;
-
-        private void Awake()
-        {
-            _dirction = Player.Player.Instance.PlayerInputHandler.PlayerWeaponInput.ProjectileDirection;
-        }
+        private float _currentLifetime;
 
         private void Update()
         {
-            transform.position += transform.up;
+            transform.position += transform.up * _speed;
+            _currentLifetime += Time.deltaTime;
+            if (_currentLifetime >= _maxLifeTime)
+                Exploed();
         }
 
         private void Exploed()
         {
-            Destroy(this);
+            Destroy(this.gameObject);
         }
-
-        //private void OnCollisionEnter(Collision collision)
-        //{
-        //    if (collision.gameObject.TryGetComponent(out Player.Player player))
-        //    {
-        //        player.DoDamage(_damage);
-        //    }
-            
-        //    Exploed();
-        //}
     }
 }
