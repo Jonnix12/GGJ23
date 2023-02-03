dd70d9cb8cee686e19407df61419da083c059ea2
Liad Halotzi <liadhalotzi@gmail.com>
2023-02-04 00:04:46 +0200

    Finishing Player animation

--- Assets/Scripts/Player/PlayerAnimationHandler.cs
+++ Assets/Scripts/Player/PlayerAnimationHandler.cs
@@ -9,23 +9,47 @@ namespace Player
         [SerializeField] List<Sprite> _playerIdleSprites; //1:Front, 2:Down, 3:Left, 4:Right
         [SerializeField] Animator _animator;
 
+        public void StopAnimation()
+        {
+            _animator.SetBool("IsMovingLeft", false);
+            _animator.SetBool("IsMovingRight", false);
+                _animator.SetBool("IsMovingUp", false);
+        }
+
         public void ChangeIdleSprite(float rotate)
         {
-            
-            if(rotate >= 135 && rotate <= -135)
+
+            if (rotate >= 135 && rotate <= -135)
+            {
                 _playerSpriteRenderer.sprite = _playerIdleSprites[0];
+                _animator.SetBool("IsMovingUp", false);
+                _animator.SetBool("IsMovingLeft", false);
+                _animator.SetBool("IsMovingRight", false);
+            }
 
             else if (rotate >= -45 && rotate <= 45)
+            {
                 _playerSpriteRenderer.sprite = _playerIdleSprites[1];
+                _animator.SetBool("IsMovingUp", true);
+                _animator.SetBool("IsMovingLeft", false);
+                _animator.SetBool("IsMovingRight", false);
+            }
 
             else if (rotate > 45 && rotate < 135)
+            {
                 _playerSpriteRenderer.sprite = _playerIdleSprites[2];
+                _animator.SetBool("IsMovingUp", false);
+                _animator.SetBool("IsMovingLeft", true);
+                _animator.SetBool("IsMovingRight", false);
+            }
 
             else if (rotate > -135 && rotate <= -45)
+            {
                 _playerSpriteRenderer.sprite = _playerIdleSprites[3];
-
-            else
-            _playerSpriteRenderer.sprite = _playerIdleSprites[0];
+                _animator.SetBool("IsMovingUp", false);
+                _animator.SetBool("IsMovingLeft", false);
+                _animator.SetBool("IsMovingRight", true);
+            }
         }
     }
 }
